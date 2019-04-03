cbd75f95ac06bd0fb32fce19be3d9f399b1a07c0
Khari <khari.lane@khalikenterprises.com>
2019-04-03 13:07:51 -0500

    Finished Timestamp. Node modules folder producing errors

---
+++ Timestamp.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const parse = require('body-parser');
+const app = module.exports = express();
+
+// parse data to JSON format with body parser
+app.use(parse.json());
+
+// Handle GET Request
+app.get('/api/timestamp/:dVal', (req, res) => {
+  let utcDate;
+  let unixDate;
+
+  const dVal = req.params.dVal; // pulls parameter from link
+
+  // format the date
+  const dStyle = {
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric'
+  };
+
+  if(isNaN(dVal)) { // handles natural dates
+    utcDate = new Date(dVal).toLocaleDateString('en-us', dStyle);
+    unixDate = new Date(dVal).getTime() * 0.001;
+  } else { // handles unix numbers
+    utcDate = new Date(dVal * 1000).toLocaleDateString('en-us', dStyle);
+    unixDate = dVal;
+  }
+  // respond with the JSON object
+  res.json({
+    unix: unixDate, 
+    utc: utcDate
+  });
+});
+
+app.listen(4000, () => console.log(`I'M LISTENGING`) );
